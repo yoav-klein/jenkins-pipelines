0579a3a4c1744bd6dde39855113754de348aba13
yoklein <yoav.klein@cyberark.com>
2024-08-27 11:33:14 +0300

    Gradle pipeline

---
+++ gradle-artifactory-pipeline/tcp-client/src/main/java/com/yoav/TCPClient.java
@@ -0,0 +1,14 @@
+
+package com.yoav;
+
+public class TCPClient {
+
+    public TCPClient() {
+        System.out.println("TCP Client CTOR");
+    }
+
+    public void openConnection() {
+        System.out.println("Opening TCP connection");
+    }
+
+}
